9e318c948c8a368bcc8251fade3196d89bc2bcc4
Iván Bosch <ivan.bosch.amengual@gmail.com>
2020-12-11 16:41:09 +0100

    Updates

--- src/Cotxe_Iván_Bosch.java
+++ src/Cotxe_Iván_Bosch.java
@@ -1,16 +1,14 @@
-public class Cotxe_Iván_Bosch extends Cotxe implements InterfaceCotxe {
+public class Cotxe_Iván_Bosch extends CotxeAbstracte implements InterfaceCotxe {
     private EstatsMotorCotxe estado=EstatsMotorCotxe.Aturat;
     private int revoluciones=0;
+
+    //Constructor
     public Cotxe_Iván_Bosch(String marca, String model, TipusCanvi tipuscanvi) {
         super(marca, model, tipuscanvi);
     }
 
-    //Constructor
-    public Cotxe_Iván_Bosch(){
-        super(null,null,null);
-    }
-
     @Override
+    //Arranca el motor y si está en marcha muestra una excepción.
     public void arrancarMotor() throws Exception {
         if (estado.equals(EstatsMotorCotxe.Aturat)){
             this.estado=EstatsMotorCotxe.EnMarxa;
@@ -20,6 +18,7 @@ public class Cotxe_Iván_Bosch extends Cotxe implements InterfaceCotxe {
     }
 
     @Override
+    //Muestra si el motor está en marcha o parado.
     public EstatsMotorCotxe comprovaMotor() {
         if (this.estado.equals(EstatsMotorCotxe.EnMarxa)){
             System.out.println("El coche está en marcha");
@@ -30,6 +29,7 @@ public class Cotxe_Iván_Bosch extends Cotxe implements InterfaceCotxe {
     }
 
     @Override
+    //Muestra las revoluciones que són 0 si el motor está parado y un número aleatorio entre 1 y 6500 si está en marcha.
     public int getRevolucions() {
         if (this.estado.equals(EstatsMotorCotxe.Aturat)){
             System.out.println("El coche está parado, así que las revoluciones son "+revoluciones);
@@ -42,6 +42,7 @@ public class Cotxe_Iván_Bosch extends Cotxe implements InterfaceCotxe {
     }
 
     @Override
+    //Para el motor si está en marcha y da una excepción si ya estaba parado.
     public void aturarMotor() throws Exception {
         if (estado.equals(EstatsMotorCotxe.EnMarxa)){
             this.estado=EstatsMotorCotxe.Aturat;
